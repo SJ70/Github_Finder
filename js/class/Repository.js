cb54bf0ff3062a1b3eb566e29ab72196ef303d42
SJ70 <seojae70@gmail.com>
2024-02-22 17:55:33 +0900

    refactor: 객체 불변 상태 고정
    - repository 클래스에 적용이 안 되어 있었음

--- js/class/Repository.js
+++ js/class/Repository.js
@@ -7,5 +7,7 @@ export default class Repository {
         this.stars = repoInfo.stargazers_count;
         this.watchers = repoInfo.watchers_count;
         this.forks = repoInfo.forks_count;
+
+        Object.freeze(this);
     }
 }
