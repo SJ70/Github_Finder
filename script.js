ca351702d94e33eca7d149f49c9c4c977130e61e
SJ70 <seojae70@gmail.com>
2024-02-23 17:56:28 +0900

    refactor: 검색 요청 대기 시간 추가

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-import { searchUsersByUserName, getUserByUserName, getRecentReposByUser } from './js/api/GithubApi.js';
-import displayReposEl from './js/element/displayReposEl.js';
+import { searchUsersByUserName } from './js/api/GithubApi.js';
 import { clearResults, displaySearchResultsEl } from './js/element/displaySearchResultsEl.js';
-import ProfileView from './js/element/ProfileView.js';
-import User from './js/class/User.js';
 
 const searchUserInputEl = document.getElementById('searchUserInput');
 
-searchUserInputEl.addEventListener('input', async (event) => {
-  const inputValue = event.target.value;
-  if (inputValue == "") {
-    clearResults();
-    return;
-  }
+let searchTimeOut = null;
 
-  const searchUserResult = await searchUsersByUserName(inputValue);
-  displaySearchResultsEl(searchUserResult);
+searchUserInputEl.addEventListener('input', async (event) => {
+
+  // 입력 후 0.5초가 지난 후에 검색 요청을 보냄
+  clearTimeout(searchTimeOut);
+  searchTimeOut = setTimeout(async () => {
+
+    const inputValue = event.target.value;
+    if (inputValue == "") {
+      clearResults();
+      return;
+    }
+  
+    const searchUserResult = await searchUsersByUserName(inputValue);
+    displaySearchResultsEl(searchUserResult);
+
+  }, 500);
+
 });
